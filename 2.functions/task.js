a3e6b57195427b6d2e650536096011101dd2dd75
Artem Artemev <Vektor-rpg@yandex.ru>
2021-12-28 20:46:29 +0700

    home work 2

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,8 +1,23 @@
 // Задание 1
 function getArrayParams(arr) {
   let min, max, sum, avg;
+  max = -Infinity;
+  min = Infinity;
+  sum = 0;
 
-  // Ваш код
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] > max){
+    max = arr[i]
+    }
+    if(arr[i] < min){
+     min = arr[i]
+	}
+    sum +=arr[i] 
+  }
+
+  avg = sum / arr.length;
+  avg = avg.toFixed(2);
+  avg = parseFloat(avg);
 
   return { min: min, max: max, avg: avg };
 }
@@ -10,22 +25,48 @@ function getArrayParams(arr) {
 // Задание 2
 function worker(arr) {
   let sum;
+  sum = 0;
 
-  // Ваш код
+  for(let i = 0; i < arr.length; i++){
+  sum += arr[i]
+  }
+  
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+  let max = 0;
+  let startSum;
 
-  // Ваш кода
-  // for ...
+  for(let i = 0; i < arrOfArr.length; i++){
+      startSum = func(arrOfArr[i]);
+
+      if(startSum > max){
+       max = startSum;
+	  }
+  }
   
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let max = -Infinity;
+  let min = Infinity;
+  let difference = 0;
+
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] > max){
+    max = arr[i]
+    }
+    if(arr[i] < min){
+     min = arr[i]
+	}
+
+    difference = min - max;
+    difference = Math.abs(difference)
+  }
+
+  return difference;
 }
